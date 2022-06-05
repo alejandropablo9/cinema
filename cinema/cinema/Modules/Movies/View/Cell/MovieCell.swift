258e7bd527b98632606a4dc396343994561b042c
Jesus Alejandro Pablo Ojeda <alejandropablo9@gmail.com>
2022-06-05 16:24:14 -0500

    Feat: Login
    Se realiza diseño de la vista login
    Se implementa servicios

---
+++ cinema/cinema/Modules/Movies/View/Cell/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  cinema
+//
+//  Created by Jesus Alejandro Pablo Ojeda on 05/06/22.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
