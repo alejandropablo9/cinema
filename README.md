6c584005f7d44216f8aaf2b26def11001bd36f0f
Jesus Alejandro Pablo Ojeda <alejandropablo9@gmail.com>
2022-06-06 10:36:15 -0500

    Doc: sea agrega documentación en readme y clases.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Test
+Implementación de inicio de sesión, consumo de servicios, persistencia local.
+## Libreria de terceros
+-  [Alamofire](https://github.com/Alamofire/Alamofire): Librería usada para el consumo de servicios web.
+- [SwiftMessages](https://github.com/SwiftKickMobile/SwiftMessages): Librería utilizada para mostrar mensajes de alerta dentro de la aplicación.
+- [RealmSwift](https://github.com/realm/realm-swift): Librería  utilizada para persistir datos dentro de la aplicación.
+- [JGProgressHUD](https://github.com/JonasGessner/JGProgressHUD): Librería para mostrar un indicador de carga.
--- cinema/cinema/Utils/NetMonitor.swift
+++ cinema/cinema/Utils/NetMonitor.swift
@@ -5,32 +5,38 @@
 //  Created by Jesus Alejandro Pablo Ojeda on 05/06/22.
 //
 import Network
+/// Enumeración con los tipos de conexión
 public enum ConnectionType {
     case wifi
     case ethernet
     case cellular
     case unknown
 }
+/// Clase que escucha el cambio de estados de la conexión a internet.
 class NetMonitor {
     static public let shared = NetMonitor()
     private var monitor: NWPathMonitor
     private var queue = DispatchQueue.global()
     var connType: ConnectionType = .wifi
     var netOn: Bool = true
+    /// Constructor de la clase
     private init() {
         self.monitor = NWPathMonitor()
         self.queue = DispatchQueue.global(qos: .background)
         self.monitor.start(queue: queue)
     }
+    /// Inicia el monitoreo de los cambios de estado.
     func startMonitoring() {
         self.monitor.pathUpdateHandler = { path in
             self.netOn = path.status == .satisfied
             self.connType = self.checkConnectionTypeForPath(path)
         }
     }
+    /// Detiene el monitoreo
     func stopMonitoring() {
         self.monitor.cancel()
     }
+    /// Regresa el tipo de estado actual.
     func checkConnectionTypeForPath(_ path: NWPath) -> ConnectionType {
         if path.usesInterfaceType(.wifi) {
             return .wifi
